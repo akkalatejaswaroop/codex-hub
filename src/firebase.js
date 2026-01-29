98125e8097119718dfc5d6fb5276f29d94354780
AKKALA TEJA SWAROOP <Tejaswaroopakkala@gmail.com>
2026-01-29 12:27:54 +0530

    Refactor Firebase configuration with empty strings
    Updated Firebase configuration to use empty strings for credentials.

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,15 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBeDuSz97J5k4UPQ0i4izS74dEmJI0knxQ",
-    authDomain: "violet-horizon.firebaseapp.com",
-    projectId: "violet-horizon",
-    storageBucket: "violet-horizon.firebasestorage.app",
-    messagingSenderId: "565187947189",
-    appId: "1:565187947189:web:e4b3f95914968d52de6cfc"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+
